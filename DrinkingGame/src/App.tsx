beb5fcb39b5ad7d1027985a887d9c697e1761258
Norskeren <30465652+Norskeren@users.noreply.github.com>
2023-02-01 23:32:07 +0100

    Fixed a smooth changing of background color.

--- DrinkingGame/src/App.tsx
+++ DrinkingGame/src/App.tsx
@@ -1,6 +1,6 @@
 import { registerRootComponent } from 'expo'
 import { StrictMode } from 'react';
-import { StyleSheet, Text, View, useWindowDimensions, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, useWindowDimensions, SafeAreaView, Animated, TouchableWithoutFeedback } from 'react-native';
 import React, {useState, useEffect} from 'react';
 import HomePage from './components/HomePage';
 
@@ -11,29 +11,64 @@ export default function App() {
   // fontscale: The scale of the font currently used. Some operating systems allow users to scale their font sizes larger or smaller for reading comfort.
   // This property will let you know what is in effect.
   // const {height, width, scale, fontScale} = useWindowDimensions();
+  
+  const [animation, setAnimation] = useState(new Animated.Value(0))
 
+  // useEffect(() => {
+  //   handleAnimation
+  // });
+  const boxInterpolation =  animation.interpolate({
+    inputRange: [0, 1],
+    outputRange:["rgb(168,54,235)" , "rgb(60,9,227)"]
+  })
+  const animatedStyle = {
+    backgroundColor: boxInterpolation
+  }
+  
+  function cycleAnimation() {
+    Animated.sequence([
+      Animated.timing(animation, {
+        toValue: 1,
+        duration: 5000,
+        delay: 1000,
+        useNativeDriver: false
+      }),
+      Animated.timing(animation, {
+        toValue: 0,
+        duration: 5000,
+        useNativeDriver: false
+      })
+    ]).start(event => {
+      if (event.finished) {
+        cycleAnimation();
+      }
+    });
+  }
+  cycleAnimation();
 
- // renders BoardContainer when player clicks start game button
- const [renderGame, setRenderGame] = useState(false)
- // will get names from HomePage into BoardContainer
- const [inGameNames, setInGameNames] = useState<string[]>([])
+  
 
   return (
     <StrictMode>
-      <SafeAreaView style={[styles.OutsideSafeViewContainer]}>
+      <Animated.View style={{...styles.box, ...animatedStyle}}>
+
+      <SafeAreaView>
+
         <View style={styles.InsideSafeViewContainer}>
           <HomePage />
         </View>
       </SafeAreaView>
-
+    </Animated.View>
     </StrictMode>
   );
 }
 
 const styles = StyleSheet.create({
-  OutsideSafeViewContainer: {
-    flex: 1,
-    backgroundColor: '#900DFF',
+
+  box:{
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#5AD2F4'
   },
   InsideSafeViewContainer: {
     borderColor: 'green',
